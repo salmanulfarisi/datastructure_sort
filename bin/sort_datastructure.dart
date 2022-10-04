acc63564ef297aeedfc204aedb60fe6a60d0a02f
Salmanul Farisi <salmanfarisi0027@gmail.com>
2022-10-04 11:51:49 +0530

    sort datastructure

---
+++ bin/sort_datastructure.dart
@@ -0,0 +1,5 @@
+import 'package:sort_datastructure/sort_datastructure.dart' as sort_datastructure;
+
+void main(List<String> arguments) {
+  print('Hello world: ${sort_datastructure.calculate()}!');
+}
---
+++ test/sort_datastructure_test.dart
@@ -0,0 +1,8 @@
+import 'package:sort_datastructure/sort_datastructure.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
